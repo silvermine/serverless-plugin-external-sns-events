5e261c060757a416b98da7e45e04cb28f993eedd
Alex Eftimie <alex.eftimie@getyourguide.com>
2016-10-21 13:21:52 +0200

    no need for permissions

--- src/index.js
+++ src/index.js
@@ -48,6 +48,10 @@ module.exports = Class.extend({
           permRef = normalizedFnName + 'LambdaPermission' + normalizedTopicName,
           permission;
 
+      if (topicName.startsWith('arn:aws:')) {
+        // do not add permissions for external functions
+        return;
+      }
       permission = {
          Type: 'AWS::Lambda::Permission',
          Properties: {
